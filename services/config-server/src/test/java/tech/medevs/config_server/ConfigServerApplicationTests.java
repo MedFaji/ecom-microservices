335e687b93a78b86a78e3f6f2942dc53b19e3980
Medevs <hello@medevs.tech>
2024-07-25 20:50:28 +0100

    initial commit

---
+++ services/config-server/src/test/java/tech/medevs/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.medevs.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
